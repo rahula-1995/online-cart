f4bddb239971b081c9bc8102ae43173211699155
Rahul <alokrockrahul@gmail.com>
2020-06-04 22:49:05 +0530

    first

--- coronakit/settings.py
+++ coronakit/settings.py
@@ -131,7 +131,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'coronawarrior/static'),
 ]
-MEDIA_ROOT = os.path.join(BASE_DIR, 'coronawarrior/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 import dj_database_url
 prod_db  =  dj_database_url.config(conn_max_age=500)
